504ad0c4986cf93ff667b2a6a6c487fe46ab1bed
driesbruylandt <dries.bruylandt@student.ehb.be>
2024-01-04 19:07:05 +0100

    Test

--- Models/User.js
+++ Models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
+// Test github
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
